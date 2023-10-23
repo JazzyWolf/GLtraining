cc68418b887a0fdc3e798a63bfe4114a805b0120
alisia.iliescu <alisia.iliescu1@gmail.com>
2023-10-23 08:56:43 +0300

    added json class in which I want to read json properties

---
+++ src/main/java/org/example/JsonClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class JsonClass {
+    public static class JsonProperty{
+        private String myProperty;
+
+        public String getMyProperty() {
+            return myProperty;
+        }
+    }
+}
